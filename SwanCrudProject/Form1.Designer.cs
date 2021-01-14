128709a9cbbde1f610fa91b88d67de08f0725e3a
Swanfreedom <43937299+Swanfreedom@users.noreply.github.com>
2021-01-14 12:22:11 +0530

    First Commit

---
+++ SwanCrudProject/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SwanCrudProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NametextBox = new System.Windows.Forms.TextBox();
+            this.PhonenotextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EmailidtextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.locationcomboBox = new System.Windows.Forms.ComboBox();
+            this.Insertbutton = new System.Windows.Forms.Button();
+            this.updatebutton = new System.Windows.Forms.Button();
+            this.Deletebutton = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(248, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(504, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bright Media Solution Pvt. Ltd.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(116, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // NametextBox
+            // 
+            this.NametextBox.Location = new System.Drawing.Point(273, 75);
+            this.NametextBox.Name = "NametextBox";
+            this.NametextBox.Size = new System.Drawing.Size(258, 22);
+            this.NametextBox.TabIndex = 0;
+            // 
+            // PhonenotextBox
+            // 
+            this.PhonenotextBox.Location = new System.Drawing.Point(273, 131);
+            this.PhonenotextBox.Name = "PhonenotextBox";
+            this.PhonenotextBox.Size = new System.Drawing.Size(258, 22);
+            this.PhonenotextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(116, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Phone No.";
+            // 
+            // EmailidtextBox
+            // 
+            this.EmailidtextBox.Location = new System.Drawing.Point(273, 187);
+            this.EmailidtextBox.Name = "EmailidtextBox";
+            this.EmailidtextBox.Size = new System.Drawing.Size(258, 22);
+            this.EmailidtextBox.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(116, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Email ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(116, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Location";
+            // 
+            // locationcomboBox
+            // 
+            this.locationcomboBox.FormattingEnabled = true;
+            this.locationcomboBox.Items.AddRange(new object[] {
+            "Delhi",
+            "Mumbai",
+            "Kerla"});
+            this.locationcomboBox.Location = new System.Drawing.Point(273, 242);
+            this.locationcomboBox.Name = "locationcomboBox";
+            this.locationcomboBox.Size = new System.Drawing.Size(258, 24);
+            this.locationcomboBox.TabIndex = 3;
+            // 
+            // Insertbutton
+            // 
+            this.Insertbutton.Location = new System.Drawing.Point(273, 286);
+            this.Insertbutton.Name = "Insertbutton";
+            this.Insertbutton.Size = new System.Drawing.Size(105, 34);
+            this.Insertbutton.TabIndex = 4;
+            this.Insertbutton.Text = "Insert";
+            this.Insertbutton.UseVisualStyleBackColor = true;
+            this.Insertbutton.Click += new System.EventHandler(this.Insertbutton_Click);
+            // 
+            // updatebutton
+            // 
+            this.updatebutton.Location = new System.Drawing.Point(426, 286);
+            this.updatebutton.Name = "updatebutton";
+            this.updatebutton.Size = new System.Drawing.Size(105, 34);
+            this.updatebutton.TabIndex = 5;
+            this.updatebutton.Text = "Update";
+            this.updatebutton.UseVisualStyleBackColor = true;
+            this.updatebutton.Click += new System.EventHandler(this.updatebutton_Click);
+            // 
+            // Deletebutton
+            // 
+            this.Deletebutton.Location = new System.Drawing.Point(579, 286);
+            this.Deletebutton.Name = "Deletebutton";
+            this.Deletebutton.Size = new System.Drawing.Size(105, 34);
+            this.Deletebutton.TabIndex = 6;
+            this.Deletebutton.Text = "Delete";
+            this.Deletebutton.UseVisualStyleBackColor = true;
+            this.Deletebutton.Click += new System.EventHandler(this.Deletebutton_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(13, 344);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(974, 245);
+            this.dataGridView.TabIndex = 7;
+            this.dataGridView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 601);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.Deletebutton);
+            this.Controls.Add(this.updatebutton);
+            this.Controls.Add(this.Insertbutton);
+            this.Controls.Add(this.locationcomboBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.EmailidtextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.PhonenotextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NametextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Employee Details";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NametextBox;
+        private System.Windows.Forms.TextBox PhonenotextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox EmailidtextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox locationcomboBox;
+        private System.Windows.Forms.Button Insertbutton;
+        private System.Windows.Forms.Button updatebutton;
+        private System.Windows.Forms.Button Deletebutton;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ SwanCrudProject/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace SwanCrudProject
+{
+    public partial class Form1 : Form
+        
+
+    {
+        BMSDETAILSDBDataContext db;
+        public Form1()
+        {
+            AppCenter.Start("ccbccd56-a393-44ea-950e-14d080ff27e3",
+                   typeof(Analytics), typeof(Crashes));
+            Analytics.TrackEvent("My custom event");
+            InitializeComponent();
+        }
+
+        private void BindGridView()
+        {
+            db = new BMSDETAILSDBDataContext();
+            dataGridView.DataSource = db.BMSDETAILs;
+        }
+
+        private void ClearTextBoxes()
+        {
+            foreach(Control ctr in this.Controls)
+            {
+                if (ctr is TextBox)
+                {
+                    TextBox txt = ctr as TextBox;
+                    txt.Clear();
+                }
+              
+            }
+            NametextBox.Focus();
+        }
+
+
+        private void Insertbutton_Click(object sender, EventArgs e)
+        {
+            if (NametextBox.Text == "" || EmailidtextBox.Text == "" || locationcomboBox.Text == "" || PhonenotextBox.Text=="")
+            {
+                MessageBox.Show("All fields are Mandatory");
+                
+            }
+            else
+            {
+                db = new BMSDETAILSDBDataContext();
+                BMSDETAIL b = new BMSDETAIL();
+                b.name = NametextBox.Text;
+                b.phoneno = int.Parse(PhonenotextBox.Text);
+                b.emailid = EmailidtextBox.Text;
+                b.location = locationcomboBox.Text;
+                db.BMSDETAILs.InsertOnSubmit(b); // data save on linq 
+                db.SubmitChanges(); // from permently infromation save to use Submitchanges
+                MessageBox.Show("Employee Infromation inserted Successfully Done!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearTextBoxes();
+                BindGridView(); 
+            }
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindGridView();
+        }
+
+        private void dataGridView_MouseClick(object sender, MouseEventArgs e)
+        {
+            NametextBox.Text = dataGridView.SelectedRows[0].Cells[1].Value.ToString();
+            PhonenotextBox.Text = dataGridView.SelectedRows[0].Cells[2].Value.ToString();
+            EmailidtextBox.Text = dataGridView.SelectedRows[0].Cells[3].Value.ToString();
+            locationcomboBox.Text = dataGridView.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        private void updatebutton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count > 0)
+            {
+                db = new BMSDETAILSDBDataContext();
+                int id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                BMSDETAIL bms = db.BMSDETAILs.FirstOrDefault(s => s.Id == id);
+                bms.name = NametextBox.Text;
+                bms.phoneno = int.Parse(PhonenotextBox.Text);
+                bms.emailid = EmailidtextBox.Text;
+                bms.location = locationcomboBox.Text;
+                db.SubmitChanges();
+                MessageBox.Show("Employee Infromation updated Successfully Done!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearTextBoxes();
+                BindGridView();
+            }
+            else
+            {
+                MessageBox.Show("Please Select for update");
+            }
+            
+
+        }
+
+        private void Deletebutton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count > 0)
+            {
+                db = new BMSDETAILSDBDataContext();
+                int id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                BMSDETAIL bms = db.BMSDETAILs.FirstOrDefault(s => s.Id == id);
+                db.BMSDETAILs.DeleteOnSubmit(bms);
+                db.SubmitChanges();
+                MessageBox.Show("Employee Infromation Deleted Successfully Done!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearTextBoxes();
+                BindGridView();
+            }
+            else
+            {
+                MessageBox.Show("Please Select for Delete");
+            }
+            
+        }
+    }
+}
